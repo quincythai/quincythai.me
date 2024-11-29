c8bfb37c2a68f44bcffd62c4fedeeec647846272
Quincy Thai <quincythai50@icloud.com>
2024-11-28 16:05:39 -0800

    Add alias routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "@assets",
+        replacement: fileURLToPath(
+          new URL("./src/shared/assets", import.meta.url)
+        ),
+      },
+      {
+        find: "@cmp",
+        replacement: fileURLToPath(
+          new URL("./src/shared/cmp", import.meta.url)
+        ),
+      },
+      {
+        find: "@stores",
+        replacement: fileURLToPath(
+          new URL("./src/shared/stores", import.meta.url)
+        ),
+      },
+      {
+        find: "@use",
+        replacement: fileURLToPath(
+          new URL("./src/shared/use", import.meta.url)
+        ),
+      },
+    ],
+  },
+});
